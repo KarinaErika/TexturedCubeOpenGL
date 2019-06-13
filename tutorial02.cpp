684d7821b171781ad9d9664571d7cfe59eb6e314
Karina <karinaerikads@hotmail.com>
2019-06-13 17:46:07 -0300

    tradução de alguns comentários do código

--- tutorial02.cpp
+++ tutorial02.cpp
@@ -20,8 +20,7 @@ using namespace glm;
 int main(void)
 {
 	// Inicializa GLFW
-	if (!glfwInit())
-	{
+	if (!glfwInit()){
 		fprintf(stderr, "Failed to initialize GLFW\n");
 		getchar();
 		return -1;
@@ -101,14 +100,15 @@ int main(void)
 	//Nosso ModelViewProjection: multiplicação de nossas 3 matrizes
 	glm::mat4 mvp = Projection * View * Model; //A multiplicação de matrizes é o contrário
 
-	//Carregue a textura usando dois métodos
+	//Carrega a textura usando dois métodos
 	//GLuint Texture = loadBMP_custom("C:/ogl-master  build/tutorial02_red_triangle.dir/Debug/uvtemplate.bmp");
 	GLuint Texture = loadDDS("C:/ogl-master  build/tutorial02_red_triangle.dir/Debug/uvtemplate.DDS");
 
 	// Get a handle for our "myTextureSampler" uniform
+	// Obtenha um identificador para o  uniforme "myTextureSampler"
 	GLuint TextureID = glGetUniformLocation(programID, "myTextureSampler");
 
-	//// Uma matriz de 3 vetores que representa 3 vértices
+	//// Uma matriz de 3 vetores que representa 3 vértices (Para triângulo) 
 	//static const GLfloat g_vertex_buffer_data[] = {
 	//	-1.0f, -1.0f, 0.0f,
 	//	 1.0f, -1.0f, 0.0f,
@@ -198,7 +198,47 @@ int main(void)
 	//};
 
 
-	// Two UV coordinatesfor each vertex. They were created with Blender.
+	//// Duas coordenadas UV para cada vértice. Eles foram criados com o Blender.
+	//static const GLfloat g_uv_buffer_data[] = {
+	//	0.583f,  0.771f,  0.014f,
+	//	0.609f,  0.115f,  0.436f,
+	//	0.327f,  0.483f,  0.844f,
+	//	0.822f,  0.569f,  0.201f,
+	//	0.435f,  0.602f,  0.223f,
+	//	0.310f,  0.747f,  0.185f,
+	//	0.597f,  0.770f,  0.761f,
+	//	0.559f,  0.436f,  0.730f,
+	//	0.359f,  0.583f,  0.152f,
+	//	0.483f,  0.596f,  0.789f,
+	//	0.559f,  0.861f,  0.639f,
+	//	0.195f,  0.548f,  0.859f,
+	//	0.014f,  0.184f,  0.576f,
+	//	0.771f,  0.328f,  0.970f,
+	//	0.406f,  0.615f,  0.116f,
+	//	0.676f,  0.977f,  0.133f,
+	//	0.971f,  0.572f,  0.833f,
+	//	0.140f,  0.616f,  0.489f,
+	//	0.997f,  0.513f,  0.064f,
+	//	0.945f,  0.719f,  0.592f,
+	//	0.543f,  0.021f,  0.978f,
+	//	0.279f,  0.317f,  0.505f,
+	//	0.167f,  0.620f,  0.077f,
+	//	0.347f,  0.857f,  0.137f,
+	//	0.055f,  0.953f,  0.042f,
+	//	0.714f,  0.505f,  0.345f,
+	//	0.783f,  0.290f,  0.734f,
+	//	0.722f,  0.645f,  0.174f,
+	//	0.302f,  0.455f,  0.848f,
+	//	0.225f,  0.587f,  0.040f,
+	//	0.517f,  0.713f,  0.338f,
+	//	0.053f,  0.959f,  0.120f,
+	//	0.393f,  0.621f,  0.362f,
+	//	0.673f,  0.211f,  0.457f,
+	//	0.820f,  0.883f,  0.371f,
+	//	0.982f,  0.099f,  0.879f
+	//};	
+	
+	//Duas coordenadas UV para cada vértice. Eles foram criados com o Blender.
 	static const GLfloat g_uv_buffer_data[] = {
 		0.000059f, 1.0f - 0.000004f,
 		0.000103f, 1.0f - 0.336048f,
@@ -239,7 +279,7 @@ int main(void)
 	};
 
 
-	//Dar este triângulo ao OpenGL, fazemos isso criando um buffer: 
+	//Dar este triângulo (ou cubo) ao OpenGL, fazemos isso criando um buffer: 
 
 	// Para o triângulo
 	GLuint vertexbuffer; //Isso irá identificar nosso buffer de vértices
@@ -287,7 +327,7 @@ int main(void)
 		//glVertexAttribPointer(
 		//	1,                                // atributo. Nenhuma razão específica para 1, mas deve corresponder ao layout no shader.
 		//	3,                                // tamanho
-		//	GL_FLOAT,                         // ipo
+		//	GL_FLOAT,                         // tipo
 		//	GL_FALSE,                         // normalizado?
 		//	0,                                // stride (passo largo)
 		//	(void*)0                          // deslocamento de buffer de matriz
@@ -296,12 +336,12 @@ int main(void)
 		glEnableVertexAttribArray(1);
 		glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
 		glVertexAttribPointer(
-			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-			2,                                // size : U+V => 2
-			GL_FLOAT,                         // type
-			GL_FALSE,                         // normalized?
-			0,                                // stride
-			(void*)0                          // array buffer offset
+			1,                                // atributo. Nenhuma razão específica para 1, mas deve corresponder ao layout no shader.
+			2,                                // tamanho : U+V => 2
+			GL_FLOAT,                         // tipo
+			GL_FALSE,                         // normalizado?
+			0,                                // stride (passo largo)
+			(void*)0                          // deslocamento de buffer de matriz
 
 		);
 
