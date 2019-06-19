24aa0b4e8634100e0e1a0de1a2d3df123b03f701
Karina <karinaerikads@hotmail.com>
2019-06-19 13:29:16 -0300

    versão com a interação com as teclas feitas

--- tutorial02.cpp
+++ tutorial02.cpp
@@ -17,7 +17,7 @@ using namespace glm;
 #include <common/shader.hpp>
 #include <common/texture.hpp>
 
-void processInput(GLFWwindow *window);
+void processInput(mat4 &mvp);
 
 int main(void)
 {
@@ -80,55 +80,19 @@ int main(void)
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
 	
-	////Matriz de projeção: Campo de visão de 45 °, relação 4: 3, faixa de exibição: 0,1 unidade <-> 100 unidades
-	//glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-	////Camera matrix
-	//glm::mat4 View = glm::lookAt(
-	//	glm::vec3(4, 3, 3), // A câmera está em (4,3,3), no World Space
-	//	glm::vec3(0, 0, 0), // e olha a origem
-	//	glm::vec3(0, 1, 0)  // Head is up (definido como 0, -1,0 para ver de cabeça para baixo)
-	//);
-	//// Matriz do modelo: uma matriz de identidade (o modelo estará na origem)
-	//glm::mat4 Model = glm::mat4(1.0f);
-
-	////Nosso ModelViewProjection: multiplicação de nossas 3 matrizes
-	//glm::mat4 mvp = Projection * View * Model; //A multiplicação de matrizes é o contrário
-
-	
-	////Ou, para uma câmera ortogonal:
-	//glm::mat4 Projection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.0f, 100.0f); //em coordenadas mundiais
-	////Camera matrix
-	//glm::mat4 View = glm::lookAt(
-	//	glm::vec3(0, 0, 3), // A câmera está em (4,3,3), no World Space
-	//	glm::vec3(0, 0, 0), // e olha a origem
-	//	glm::vec3(0, 1, 0)  // Head is up (definido como 0, -1,0 para ver de cabeça para baixo)
-	//);
-	//// Matriz do modelo: uma matriz de identidade (o modelo estará na origem)
-	//glm::mat4 Model = glm::mat4(1.0f);
-
-	////Nosso ModelViewProjection: multiplicação de nossas 3 matrizes
-	//glm::mat4 mvp = Projection * View * Model; //A multiplicação de matrizes é o contrário
-
-
-
-	//Obliqua
-	glm::mat4 oblique = glm::mat4(1.0f);
-	oblique[0][2] = -1 * (glm::cos(3.14 / 4));
-	glm::mat4 Projection = oblique * ortho(-3.0f, 3.0f, -3.0f, 3.0f, -0.1f, 100.0f);
+	//Matriz de projeção: Campo de visão de 45 °, relação 4: 3, faixa de exibição: 0,1 unidade <-> 100 unidades
+	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+	//Camera matrix
 	glm::mat4 View = glm::lookAt(
-			glm::vec3(10, 0, 3), // A câmera está em (4,3,3), no World Space
-			glm::vec3(0, 0, 0), // e olha a origem
-			glm::vec3(0, 1, 0)  // Head is up (definido como 0, -1,0 para ver de cabeça para baixo)
-		);
+		glm::vec3(4, 3, 3), // A câmera está em (4,3,3), no World Space
+		glm::vec3(0, 0, 0), // e olha a origem
+		glm::vec3(0, 1, 0)  // Head is up (definido como 0, -1,0 para ver de cabeça para baixo)
+	);
 	// Matriz do modelo: uma matriz de identidade (o modelo estará na origem)
 	glm::mat4 Model = glm::mat4(1.0f);
 
 	//Nosso ModelViewProjection: multiplicação de nossas 3 matrizes
 	glm::mat4 mvp = Projection * View * Model; //A multiplicação de matrizes é o contrário
-	//Fim obliqua
-
-
-
 
 	//Carrega a textura usando dois métodos
 	//GLuint Texture = loadBMP_custom("C:/ogl-master  build/tutorial02_red_triangle.dir/Debug/uvtemplate.bmp");
@@ -228,46 +192,6 @@ int main(void)
 	//};
 
 
-	//// Duas coordenadas UV para cada vértice. Eles foram criados com o Blender.
-	//static const GLfloat g_uv_buffer_data[] = {
-	//	0.583f,  0.771f,  0.014f,
-	//	0.609f,  0.115f,  0.436f,
-	//	0.327f,  0.483f,  0.844f,
-	//	0.822f,  0.569f,  0.201f,
-	//	0.435f,  0.602f,  0.223f,
-	//	0.310f,  0.747f,  0.185f,
-	//	0.597f,  0.770f,  0.761f,
-	//	0.559f,  0.436f,  0.730f,
-	//	0.359f,  0.583f,  0.152f,
-	//	0.483f,  0.596f,  0.789f,
-	//	0.559f,  0.861f,  0.639f,
-	//	0.195f,  0.548f,  0.859f,
-	//	0.014f,  0.184f,  0.576f,
-	//	0.771f,  0.328f,  0.970f,
-	//	0.406f,  0.615f,  0.116f,
-	//	0.676f,  0.977f,  0.133f,
-	//	0.971f,  0.572f,  0.833f,
-	//	0.140f,  0.616f,  0.489f,
-	//	0.997f,  0.513f,  0.064f,
-	//	0.945f,  0.719f,  0.592f,
-	//	0.543f,  0.021f,  0.978f,
-	//	0.279f,  0.317f,  0.505f,
-	//	0.167f,  0.620f,  0.077f,
-	//	0.347f,  0.857f,  0.137f,
-	//	0.055f,  0.953f,  0.042f,
-	//	0.714f,  0.505f,  0.345f,
-	//	0.783f,  0.290f,  0.734f,
-	//	0.722f,  0.645f,  0.174f,
-	//	0.302f,  0.455f,  0.848f,
-	//	0.225f,  0.587f,  0.040f,
-	//	0.517f,  0.713f,  0.338f,
-	//	0.053f,  0.959f,  0.120f,
-	//	0.393f,  0.621f,  0.362f,
-	//	0.673f,  0.211f,  0.457f,
-	//	0.820f,  0.883f,  0.371f,
-	//	0.982f,  0.099f,  0.879f
-	//};	
-	
 	//Duas coordenadas UV para cada vértice. Eles foram criados com o Blender.
 	static const GLfloat g_uv_buffer_data[] = {
 		0.000059f, 1.0f - 0.000004f,
@@ -329,63 +253,14 @@ int main(void)
 
 	do {
 
-		//processInput(window);
-
 		// Limpa a tela, o que deixará com o fundo azul escuro por causa da definição anterior
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Informando ao opengl para usar o shader
 		glUseProgram(programID);
 
-
-		//if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-		//	
-		//	glm::mat4 oblique = glm::mat4(1.0f);
-
-		//	oblique[0][2] = -1 * (glm::cos(3.14 / 4));
-
-		//	glm::mat4 Projection = oblique * ortho(-3.0f, 3.0f, -3.0f, 3.0f, -0.1f, 100.0f);
-
-		//	// Camera matrix
-		//	glm::mat4 View = glm::lookAt(
-		//		glm::vec3(10, 0, 3), // A câmera está em (4,3,3), no World Space
-		//		glm::vec3(0, 0, 0), // e olha a origem
-		//		glm::vec3(0, 1, 0)  // Head is up (definido como 0, -1,0 para ver de cabeça para baixo)
-		//	);
-
-		//
-		//}
-
-		//if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-
-		//	glm::mat4 Projection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.0f, 100.0f); //em coordenadas mundiais
-
-		//	// Camera matrix
-		//	glm::mat4 View = glm::lookAt(
-		//		glm::vec3(0, 0, 3), // A câmera está em (4,3,3), no World Space
-		//		glm::vec3(0, 0, 0), // e olha a origem
-		//		glm::vec3(0, 1, 0)  // Head is up (definido como 0, -1,0 para ver de cabeça para baixo)
-		//	);
-
-		//	
-
-		//
-		//}
-
-		//if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-		//	//Inicio perspective
-		////Matriz de projeção: Campo de visão de 45 °, relação 4: 3, faixa de exibição: 0,1 unidade <-> 100 unidades
-		//	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-		//	// Camera matrix
-		//	glm::mat4 View = glm::lookAt(
-		//		glm::vec3(4, 3, 3), // A câmera está em (4,3,3), no World Space
-		//		glm::vec3(0, 0, 0), // e olha a origem
-		//		glm::vec3(0, 1, 0)  // Head is up (definido como 0, -1,0 para ver de cabeça para baixo)
-		//	);
-
-		//	//Fim perspective
-		//}
-
+		//Verifica qual projeção o cubo aparecerá de acordo com a tecla que o usuário clicar
+		processInput(mvp);
 
 		// Envia nossa transformação para o shader atualmente vinculado, no uniforme "MVP"
 		// Isso é feito no loop principal, já que cada modelo terá uma matriz MVP diferente (pelo menos para a parte M)
@@ -460,39 +335,58 @@ int main(void)
 }
 
 
-void processInput(GLFWwindow *window){
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-		glm::mat4 oblique = glm::mat4(1.0f);
+void processInput(mat4 &mvp){
+	glm::mat4 oblique;
+	glm::mat4 Projection;
+	glm::mat4 View;
+	//// Matriz do modelo: uma matriz de identidade (o modelo estará na origem)
+	glm::mat4 Model = glm::mat4(1.0f);
+
+	//Projeção Oblíqua
+	if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS) {
+		oblique = glm::mat4(1.0f);
 
 		oblique[0][2] = -1 * (glm::cos(3.14 / 4));
 
-		glm::mat4 Projection = oblique * ortho(-3.0f, 3.0f, -3.0f, 3.0f, -0.1f, 100.0f);
+		Projection = oblique * ortho(-3.0f, 3.0f, -3.0f, 3.0f, -0.1f, 100.0f);
 
 		// Camera matrix
-		glm::mat4 View = glm::lookAt(
+		 View = glm::lookAt(
 			glm::vec3(10, 0, 3), // A câmera está em (4,3,3), no World Space
 			glm::vec3(0, 0, 0), // e olha a origem
 			glm::vec3(0, 1, 0)  // Head is up (definido como 0, -1,0 para ver de cabeça para baixo)
 		);
+		 mvp = Projection * View * Model;
 	} 
 
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+	//Projeção Ortogonal
+	if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS) {
 		// Camera matrix
-		glm::mat4 View = glm::lookAt(
+		View = glm::lookAt(
 			glm::vec3(0, 0, 3), // A câmera está em (4,3,3), no World Space
 			glm::vec3(0, 0, 0), // e olha a origem
 			glm::vec3(0, 1, 0)  // Head is up (definido como 0, -1,0 para ver de cabeça para baixo)
 		);
 
-		glm::mat4 Projection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.0f, 100.0f); //em coordenadas mundiais
+		Projection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.0f, 100.0f); //em coordenadas mundiais
+		mvp = Projection * View * Model;
 	}
 
-}
+	//Projeção perspectiva
+	if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS) {
+		//Matriz de projeção: Campo de visão de 45 °, relação 4: 3, faixa de exibição: 0,1 unidade <-> 100 unidades
+		glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+		// Camera matrix
+		glm::mat4 View = glm::lookAt(
+			glm::vec3(4, 3, 3), // A câmera está em (4,3,3), no World Space
+			glm::vec3(0, 0, 0), // e olha a origem
+			glm::vec3(0, 1, 0)  // Head is up (definido como 0, -1,0 para ver de cabeça para baixo)
+		);
 
-/*
-cubo:
-projeções: ortogonal, obliqua, pespectiva
-câmera: 
-Textura: 
-*/
+		mvp = Projection * View * Model;
+	}
+
+
+	
+}
 
